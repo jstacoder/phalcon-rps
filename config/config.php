b0deafc7fbd9eca14e2c1c4718a6fce4fe03d613
kyle roux <kyle@level2designs.com>
2015-08-10 16:09:22 -0700

    added htaccess

--- config/config.php
+++ config/config.php
@@ -2,16 +2,40 @@
 
 defined('APP_PATH') || define('APP_PATH', realpath('.'));
 
-return new \Phalcon\Config(array(
-
-    'database' => array(
+function get_data_array($url){
+    $result = array(
+        'adapter'=>'Mysql',
+        'charset'=>'utf-8'
+    );
+    $url = explode('mysql://',$url)[1];
+    $auth = explode('@',$url)[0];
+    $hst = explode('@',$url)[1];
+    $host = explode('/',$hst)[0];
+    $db = explode('/',$hst)[1];
+    $db = explode('?',$db)[0];
+    $_auth = explode(':',$auth);
+    $result['username'] = $_auth[0];
+    $result['password'] = $_auth[1];
+    $result['host'] = $host;
+    $result['dbname'] = $db;
+    return $result;
+}
+$cfg = array(
         'adapter'    => 'Mysql',
         'host'       => 'localhost',
         'username'   => 'rps2',
         'password'   => 'rps2',
         'dbname'     => 'rps2',
         'charset'    => 'utf8',
-    ),
+);
+
+if(isset($_SERVER['CLEARDB_DATABASE_URL'])){
+    $cfg = get_data_array($_SERVER['CLEARDB_DATABASE_URL']);
+}
+
+return new \Phalcon\Config(array(
+
+    'database' => $cfg,
 
     'application' => array(
         'modelsDir'      => APP_PATH . '/models/',
